6b73796d48f7eb3678a9de4d2f3f99f9c1cbab24
SaT-hisH <sathishk@apptomate.co>
2021-08-17 17:35:22 +0530

    messege

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import {
+  Divider,
+  Input,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import TextField from "@material-ui/core/TextField";
@@ -8,172 +15,85 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import Peer from "simple-peer";
 import io from "socket.io-client";
 import "./App.css";
+//"https://videocall-be.herokuapp.com/"
+const socket = io.connect("http://localhost:5000/");
 
-const socket = io.connect("https://videocall-be.herokuapp.com/");
 function App() {
-  const [me, setMe] = useState("");
-  const [stream, setStream] = useState();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAccepted] = useState(false);
-  const [idToCall, setIdToCall] = useState("");
-  const [callEnded, setCallEnded] = useState(false);
-  const [name, setName] = useState("");
-  const myVideo = useRef();
-  const userVideo = useRef();
-  const connectionRef = useRef();
-
+  const [myName, setmyName] = useState("");
+  const [message, setmessage] = useState("");
+  const [myId, setmyId] = useState("");
+  const [error, seterror] = useState("");
+  const [senderId, setsenderId] = useState("");
+  const [listmessage, setlistmessage] = useState([]);
   useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        setStream(stream);
-        myVideo.current.srcObject = stream;
-      });
-
-    socket.on("me", (id) => {
-      setMe(id);
-      console.log(id, "id");
+    socket.on("self", (arg) => {
+      console.log(arg);
+      setmyId(arg);
     });
 
-    socket.on("callUser", (data) => {
-      setReceivingCall(true);
-      setCaller(data.from);
-      setName(data.name);
-      setCallerSignal(data.signal);
+    socket.on("callUser", (arg) => {
+      setlistmessage((val) => [...val, arg]);
     });
   }, []);
 
-  const callUser = (id) => {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream: stream,
-    });
-    peer.on("signal", (data) => {
-      socket.emit("callUser", {
-        userToCall: id,
-        signalData: data,
-        from: me,
-        name: name,
-      });
-    });
-    peer.on("stream", (stream) => {
-      userVideo.current.srcObject = stream;
-    });
-    socket.on("callAccepted", (signal) => {
-      setCallAccepted(true);
-      peer.signal(signal);
-    });
-
-    connectionRef.current = peer;
+  const enterName = (e) => {
+    setmyName(e.target.value);
   };
 
-  const answerCall = () => {
-    setCallAccepted(true);
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
-    });
-    peer.on("signal", (data) => {
-      socket.emit("answerCall", { signal: data, to: caller });
-    });
-    peer.on("stream", (stream) => {
-      userVideo.current.srcObject = stream;
-    });
+  const sendMessage = () => {
+    setlistmessage((val) => [...val, { name: "you", message: message }]);
 
-    peer.signal(callerSignal);
-    connectionRef.current = peer;
+    socket.emit("callUser", { id: senderId, message: message, name: myName });
+    setmessage("");
   };
 
-  const leaveCall = () => {
-    setCallEnded(true);
-    connectionRef.current.destroy();
+  const onchangemessage = (e) => {
+    setmessage(e.target.value);
+    seterror("");
+  };
+
+  const senderHandle = (e) => {
+    setsenderId(e.target.value);
   };
 
   return (
     <>
-      <h1 style={{ textAlign: "center", color: "#fff" }}>Zoomish</h1>
-      <div className="container">
-        <div className="video-container">
-          <div className="video">
-            {stream && (
-              <video
-                playsInline
-                muted
-                ref={myVideo}
-                autoPlay
-                style={{ width: "300px" }}
-              />
-            )}
-          </div>
-          <div className="video">
-            {callAccepted && !callEnded ? (
-              <video
-                playsInline
-                ref={userVideo}
-                autoPlay
-                style={{ width: "300px" }}
-              />
-            ) : null}
-          </div>
-        </div>
-        <div className="myId">
-          <TextField
-            id="filled-basic"
-            label="Name"
-            variant="filled"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            style={{ marginBottom: "20px" }}
-          />
-          <CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-            <Button
-              variant="contained"
-              color="primary"
-              startIcon={<AssignmentIcon fontSize="large" />}
-            >
-              Copy ID
-            </Button>
-          </CopyToClipboard>
-
-          <TextField
-            id="filled-basic"
-            label="ID to call"
-            variant="filled"
-            value={idToCall}
-            onChange={(e) => setIdToCall(e.target.value)}
-          />
-          <div className="call-button">
-            {callAccepted && !callEnded ? (
-              <Button variant="contained" color="secondary" onClick={leaveCall}>
-                End Call
-              </Button>
-            ) : (
-              <IconButton
-                color="primary"
-                aria-label="call"
-                onClick={() => callUser(idToCall)}
-              >
-                <PhoneIcon fontSize="large" />
-              </IconButton>
-            )}
-            {idToCall}
-          </div>
-        </div>
-        <div>
-          {receivingCall && !callAccepted ? (
-            <div className="caller">
-              <h1>{name} is calling...</h1>
-              <Button variant="contained" color="primary" onClick={answerCall}>
-                Answer
-              </Button>
-            </div>
-          ) : null}
-        </div>
-      </div>
+      <Input
+        placeholder="Enter your name"
+        name="myName"
+        value={myName}
+        type="text"
+        onChange={enterName}
+      />
+      <Input
+        placeholder="Enter sender id"
+        name="senderId"
+        value={senderId}
+        type="text"
+        onChange={senderHandle}
+      />
+      <Input
+        name="message"
+        value={message}
+        onChange={onchangemessage}
+        placeholder="Enter your message"
+        type="text"
+      />
+      {error}
+      <Button onClick={sendMessage}>Send</Button>
+      yourId:{myId}
+      {listmessage.map((mes) => {
+        return (
+          <>
+            <List component="nav" aria-label="mailbox folders">
+              <ListItem button>
+                <ListItemText primary={mes.name} secondary={mes.message} />
+              </ListItem>
+              <Divider light />
+            </List>
+          </>
+        );
+      })}
     </>
   );
 }
